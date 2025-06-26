f7b68abcb168adb96366002dd14a8c97ac644ba6
Vicente <nunithefanoftheyogscast@gmail.com>
2025-06-26 17:47:07 -0600

    faster atoi

--- atoi.py
+++ atoi.py
@@ -1,24 +1,23 @@
-# Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer.
-
 class Solution:
     def myAtoi(self, s: str) -> int:
-        i,n = 0, len(s)
-        sign = 1
-        result = 0
-        while i < n and s[i] == " ":
-            i+= 1
-        if i<n and s[i] == "-" or s[i] == "+":
-            sign = -1 if s[i] == "-" else 1
+        s = s.strip() 
+        if not s:
+            return 0
+
+        sign, i, res = 1, 0, 0
+        if s[0] == '-':
+            sign = -1
+            i += 1
+        elif s[0] == '+':
             i += 1
 
-        while i < n and s[i].isdigit():
-            result = result * 10 + int(s[i])
-            if result * sign > 2**31 - 1:
+        while i < len(s) and s[i].isdigit():
+            res = res * 10 + int(s[i])
+            if sign * res > 2**31 - 1:
                 return 2**31 - 1
-            if result * sign < -2**31:
+            if sign * res < -2**31:
                 return -2**31
+
             i += 1
-        return result * sign
-    
-s = Solution()
-print(s.myAtoi(" -042"))
+
+        return sign * res
